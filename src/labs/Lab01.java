9c277b0afb5974e9b471b857efdfa80bd450e283
vxs5537 <89875037+vxs5537@users.noreply.github.com>
2022-02-09 15:51:21 -0600

    hola
    i have brain dead

--- src/labs/Lab01.java
+++ src/labs/Lab01.java
@@ -1,6 +1,6 @@
 package labs;
 
-
+import java.util.Scanner;
 
 public class Lab01 {
 
@@ -16,7 +16,12 @@ public class Lab01 {
 
 	
 	public static void problem01() {
-		
+		Scanner inKey = new Scanner(System.in);	
+		System.out.println("Choose a positive integer");
+		int userNum = inKey.nextInt();
+		System.out.println("Choose a positive integer");
+		int userNum2 = inKey.nextInt();
+		System.out.println(userNum + "^" + userNum2 + " " + Math.pow(userNum, userNum2));
 		
 		
 	}
@@ -25,6 +30,10 @@ public class Lab01 {
 	
 	
 	public static void problem02() {
+		Scanner inKey = new Scanner(System.in);	
+		System.out.println("Choose a positive integer");
+		int userNum = inKey.nextInt();
+		System.out.println("The square root of " + userNum + " is " + Math.sqrt(userNum));
 		
 		
 		
@@ -34,8 +43,12 @@ public class Lab01 {
 	
 
 	public static void problem03() {
-		
-		
+		Scanner inKey = new Scanner(System.in);	
+		System.out.println("What is the side A of a right triangle");
+		int userNum = inKey.nextInt();
+		System.out.println("What is the side B of a rigth triangle ");
+		int userNum2 = inKey.nextInt();
+		System.out.println("Hypotenuse = " + Math.sqrt(Math.pow(userNum, 2)+(Math.pow(userNum2, 2))));
 		
 
 	}
@@ -44,11 +57,31 @@ public class Lab01 {
 	
 	
 	public static void problem04() {
+		int max = Integer.MIN_VALUE; 
+		int min = Integer.MAX_VALUE; 
+		Scanner inKey1 = new Scanner(System.in);
+		int userNum = 1;
+		boolean not0 = true;
+		while (not0){
+			System.out.println("Choose a integer, choose 0 when your done");
+			userNum = inKey1.nextInt();
+			if (userNum == 0) {
+				not0=false;
+			}
+			else {
+				if (userNum > max) {
+					max = userNum;
+				}
+				else if (userNum < min) {
+					min = userNum;
+				}
+			}
+			
 		
-		
-		
+		}
+		System.out.println(max);
+		System.out.println(min + "\n");
 	}
-	
 	
 	
 	
--- src/labs/Lab02.java
+++ src/labs/Lab02.java
@@ -1,6 +1,6 @@
 package labs;
 
-
+import java.util.Scanner;
 
 public class Lab02 {
 
@@ -16,8 +16,26 @@ public class Lab02 {
 
 	
 	public static void problem01() {
-		
-		
+		Scanner inKey = new Scanner(System.in);	
+		System.out.println("Choose a positive integer");
+		int userNum = inKey.nextInt();
+		System.out.println("Choose a positive integer");
+		int userNum2 = inKey.nextInt();
+		int max = Integer.MIN_VALUE; 
+		int min = Integer.MAX_VALUE; 
+		int x1=userNum;
+		int x2=userNum2;
+		max = Math.max(max, x1);
+		max = Math.max(max, x2);
+		min = Math.min(min, x1);
+		min = Math.min(min, x2);
+		int random = max-min+1;
+		int random2 = min;
+		for (int i = 0; i<10; i++) {
+			int computer = (int) (Math.random() * random + random2);
+			System.out.print(computer + " ");
+		}
+		System.out.println("");
 		
 		
 	}
@@ -26,7 +44,13 @@ public class Lab02 {
 	
 	
 	public static void problem02() {
+		Scanner inKey = new Scanner(System.in);
+		System.out.println("Choose radius of cylinder");
+		int radius = inKey.nextInt();
+		System.out.println("Choose height of cylinder");
+		int height = inKey.nextInt();
 		
+		System.out.println(Math.pow(radius, 2) * height * Math.PI);
 		
 		
 	}
@@ -36,6 +60,17 @@ public class Lab02 {
 
 	public static void problem03() {
 
+		Scanner inKey = new Scanner(System.in);
+		System.out.print("enter x1: ");
+		double x1 = inKey.nextDouble();
+		System.out.print("enter y1: ");
+		double y1 = inKey.nextDouble();
+		System.out.print("enter x2: ");
+		double x2 = inKey.nextDouble();
+		System.out.print("enter y2: ");
+		double y2 = inKey.nextDouble();
+		
+		System.out.println( Math.sqrt(Math.pow(x1-x2, 2) + Math.pow(y1-y2, 2)));
 		
 		
 	}
@@ -45,7 +80,17 @@ public class Lab02 {
 	
 	public static void problem04() {
 		
+		Scanner inKey = new Scanner(System.in);
+		System.out.print("enter a: ");
+		double a = inKey.nextDouble();
+		System.out.print("enter b: ");
+		double b = inKey.nextDouble();
+		System.out.print("enter c: ");
+		double c = inKey.nextDouble();
 		
+		System.out.println("x1 = " + ((-1*b + Math.sqrt(Math.pow(b, 2)-4*a*c)) / (2*a) ));
+		System.out.println("x2 = " + ((-1*b - Math.sqrt(Math.pow(b, 2)-4*a*c)) / (2*a) ));
+
 		
 		
 	}
